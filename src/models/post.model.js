ef3a30e0554663a00d0b1bd6906029d9fdeb277b
Ken <kennyk3n@live.com>
2020-12-15 23:50:05 -0500

    update models

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,24 +1,26 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const PostSchema = new Schema({
-  title:{type: String, required: true},
-  byline:{type: String},
-  author:{type: Schema.Types.ObjectId, ref: 'User', required: true},
-  body:{type: String, required: true},
-  publish:{type: Boolean ,required: true},
-  publishDate:{type: Date}
-}, {timestamps: true});
+const PostSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    byline: { type: String },
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    body: { type: String, required: true },
+    publish: { type: Boolean, required: true },
+    publishDate: { type: Date },
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  },
+  { timestamps: true },
+);
 
-PostSchema
-  .virtual('url')
-  .get(function(){
-    return '/posts/post/' + this._id;
-  });
-  
-PostSchema.pre('remove', function(next){
-  this.model("Comment").deleteMany({post:this._id}, next);
+PostSchema.virtual('url').get(function () {
+  return '/posts/post/' + this._id;
 });
-  
+
+PostSchema.pre('remove', function (next) {
+  this.model('Comment').deleteMany({ post: this._id }, next);
+});
+
 const Post = mongoose.model('Post', PostSchema);
 export default Post;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,46 +4,61 @@ import bcryptjs from 'bcryptjs';
 const { Schema } = mongoose;
 const SALT_WORK_FACTOR = 10;
 
+const UserSchema = new Schema(
+  {
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    accessLevel: {
+      type: String,
+      enum: ['Viewer', 'Author'],
+      default: 'Author',
+    },
+    posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  },
+  { timestamps: true },
+);
 
-const UserSchema = new Schema({
-  username:{type: String, unique: true, required: true},
-  password:{type: String, required: true},
-  name:{type: String, required: true},
-  accessLevel:{type: String, enum:["Viewer","Author"], default: "Author"}
-}, {timestamps:true})
+UserSchema.query.byUsername = function searchByUsername(username) {
+  return this.where({ username: username });
+};
 
-UserSchema
-  .virtual('url')
-  .get(function(){
-    return "/authors/author/" + this._id;
-  });
-  
-UserSchema.pre('save', function(next) {
-  let user = this;
+UserSchema.virtual('url').get(function getAuthorUrl() {
+  return `/authors/author/${this._id}`;
+});
+
+UserSchema.pre('save', async function hashPassword(next) {
+  const user = this;
   if (!user.isModified('password')) {
     return next();
   }
-  bcryptjs.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) { return next(err) }
-    bcryptjs.hash(user.password, salt, function(err, hash) {
-      if (err) { return next(err) }
-      user.password = hash;
-      next();
-    });
-  });
+  try {
+    const salt = await bcryptjs.genSalt(SALT_WORK_FACTOR);
+    const hash = await bcryptjs.hash(user.password, salt);
+
+    user.password = hash;
+  } catch (error) {
+    return next(error);
+  }
+  return next();
 });
 
-UserSchema.pre('remove', function(next){
-  this.model('Post').deleteMany({user: this._id}, next)
+UserSchema.pre('remove', function deleteAllPosts(next) {
+  this.model('Post').deleteMany({ user: this._id }, next);
 });
 
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcryptjs.compare(candidatePassword, this.password, function(err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
+UserSchema.methods.comparePassword = async function comparePassword(
+  candidatePassword,
+  next,
+) {
+  try {
+    const isMatch = await bcryptjs.compare(candidatePassword, this.password);
+    return next(null, isMatch);
+  } catch (error) {
+    return next(error);
+  }
 };
 
-
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model('User', UserSchema);
 export default User;
